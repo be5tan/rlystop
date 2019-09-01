ab7a6a8dec913542c13e7bbc7dae96ea27994547
be5tan <be5tan@posteo.de>
2019-09-01 10:30:37 +0200

    Defined filter estimator

---
+++ R/fEst.R
@@ -0,0 +1,20 @@
+filter_est <- function (m, Y, lambda, filt = c("cutoff")) {
+  #' Filter estimator
+  #'
+  #' Computes the filter estimator with stopping index \code{m} for given data
+  #' \code{Y} and diagonal design matrix \code{lambda}.
+  #' 
+  #' @param m Integer stopping index.
+  #' @param Y Numeric vector of observed data.
+  #' @param lambda Numeric vector of decreasing, strictly positive entries of
+  #'   the diagonal design matrix.
+  #' @param filt Character string giving the filter to be used.
+  #'
+  #' @return Returns a numeric vector estimating the underlying signal.
+  #'
+  #' @export
+  D <- length(Y)
+  mu_hat <- rep(0, D)
+  mu_hat[1:m] <- Y[1:m] / lambda[1:m]
+  return(mu_hat) 
+}
