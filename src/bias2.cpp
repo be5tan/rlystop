3ce652f31048b099b0b21b07b18ab200fda2b1d9
be5tan <be5tan@posteo.de>
2019-09-01 19:20:14 +0200

    Add bias variance decomposition

---
+++ src/bias2.cpp
@@ -0,0 +1,30 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' Squared bias
+//' 
+//' Computes the squared bias of the filter estimator a given diagonal design
+//' matrix \code{lambda} and signal ' \code{mu}.
+//'
+//' @param lambda Vector of decreasing, strictly positive entries of the
+//'   diagonal design matrix.
+//' @param mu Vector valued input signal. 
+//' @param alpha Numeric smoothing parameter.
+//' @param filt Character string giving the filter to be used.
+//'
+//' @return Returns all values of the squared bias in a double vector.
+//'
+//' @export
+// [[Rcpp::export]]
+NumericVector bias2(NumericVector lambda, NumericVector mu, double alpha
+        = -1.0, std::string filt = "cutoff") {
+    int D = mu.length();
+    NumericVector B2(D);
+    B2[0] = sum(pow(lambda, 2 + 2 * alpha) * pow(mu, 2)) -
+        pow(lambda[0], 2 + 2 * alpha) * pow(mu[0], 2);
+    for (int m = 1; m < D; ++m) {
+        B2[m] = B2[m - 1] -
+            pow(lambda[m], 2 + 2 * alpha) * pow(mu[m], 2);
+    }
+    return(B2);
+}
---
+++ src/variance.cpp
@@ -0,0 +1,27 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' Variance
+//' 
+//' Computes the variance of the filter estimator a given diagonal design
+//' matrix \code{lambda} and noise level delta.
+//'
+//' @param lambda Vector of decreasing, strictly positive entries of the
+//'   diagonal design matrix.
+//' @param alpha Numeric smoothing parameter.
+//' @param filt Character string giving the filter to be used.
+//'
+//' @return Returns all values of the variancee in a double vector.
+//'
+//' @export
+// [[Rcpp::export]]
+NumericVector variance(NumericVector lambda, double delta, double alpha = -1.0,
+        std::string filt = "cutoff") {
+    int D = lambda.length();
+    NumericVector V(D);
+    V[0] = pow(lambda[0], 2 * alpha) * pow(delta, 2);
+    for (int m = 1; m < D; ++m) {
+        V[m] = V[m - 1] + pow(lambda[m], 2 * alpha) * pow(delta, 2);
+    }
+    return(V);
+}
